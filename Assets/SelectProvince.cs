b063ba97fdb7079eb04e4b4a3881c5e87c20cd10
Sail <sailrecycle@gmail.com>
2025-03-19 01:03:38 +0900

    add texture/raycasting

---
+++ Assets/SelectProvince.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class SelectProvince : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetMouseButton(0))
+            return;
+
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            return;
+
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+            return;
+
+        Texture2D tex = rend.material.mainTexture as Texture2D;
+        Vector2 pixelUV = hit.textureCoord;
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+
+        Debug.Log(tex.GetPixel((int)pixelUV.x, (int)pixelUV.y));
+        tex.Apply();
+    }
+}
