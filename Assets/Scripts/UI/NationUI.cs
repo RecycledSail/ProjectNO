442845eeb9abbb8caf100c8e7ae9181fb2621281
Sail <sailrecycle@gmail.com>
2025-06-30 00:38:25 +0900

    add right-side buttons & subuis in NationUI

--- Assets/Scripts/UI/NationUI.cs
+++ Assets/Scripts/UI/NationUI.cs
@@ -6,8 +6,23 @@ public class NationUI : MonoBehaviour
 {
     public GameObject uiPanel;  // Nation UI 패널
     public TMP_Text nationNameText; // Nation 이름 표시 텍스트
+
+    //Province List 관련
     public Transform provinceListParent; // Province 목록이 들어갈 부모 객체
     public GameObject provinceItemPrefab; // Province 버튼 프리팹
+
+    //Nation stats 관련
+    public Transform nationStatsParent;
+    public TMP_Text nationStatsText;
+
+    // Panels to change
+    public List<GameObject> subUIs;
+
+    private GameObject currentOpenSubUI;
+
+    private Nation currentNation;
+
+    private int delayedUpdate;
 
     // 싱글톤 인스턴스 (다른 스크립트에서 쉽게 접근 가능)
     private static NationUI _instance;
@@ -40,21 +55,56 @@ public class NationUI : MonoBehaviour
 
     private void Start()
     {
+        for(int i=0; i<subUIs.Count; i++)
+        {
+            if(i != 0)
+            {
+                subUIs[i].SetActive(false);
+            }
+            else
+            {
+                subUIs[i].SetActive(true);
+                currentOpenSubUI = subUIs[i];
+            }
+        }
         uiPanel.SetActive(false); // 처음에는 UI를 숨김
+        currentNation = null;
+        delayedUpdate = 0;
     }
 
     private void Update()
     {
+        delayedUpdate = (delayedUpdate + 1) % 5;
+        if(delayedUpdate == 0 && currentNation != null)
+        {
+            InitProvinceList();
+            InitNationStats();
+        }
     }
+
+
     /// <summary>
     /// 특정 Nation의 UI를 열고 그에 속한 Province 목록을 표시합니다.
     /// </summary>
+    /// <param name="nation">선택한 국가</param>
     public void OpenNationUI(Nation nation)
     {
 
         // Nation 이름 표시
         nationNameText.text = nation.name;
+        currentNation = nation;
 
+        InitProvinceList();
+        InitNationStats();
+
+        UIManager.Instance.ReplacePopUp(gameObject);
+    }
+
+    /// <summary>
+    /// Province의 목록을 해당 nation의 province들로 초기화한다.
+    /// </summary>
+    public void InitProvinceList()
+    {
         // 기존 리스트 정리
         foreach (Transform child in provinceListParent)
         {
@@ -62,7 +112,7 @@ public class NationUI : MonoBehaviour
         }
 
         // Nation에 속한 Province 추가
-        if (GlobalVariables.INITIAL_PROVINCES.TryGetValue(nation.name, out List<string> provinces))
+        if (GlobalVariables.INITIAL_PROVINCES.TryGetValue(currentNation.name, out List<string> provinces))
         {
             foreach (string provinceName in provinces)
             {
@@ -76,8 +126,29 @@ public class NationUI : MonoBehaviour
                 }
             }
         }
+    }
 
-        UIManager.Instance.ReplacePopUp(gameObject);
+    /// <summary>
+    /// Province의 목록을 해당 nation의 province들로 초기화한다.
+    /// </summary>
+    public void InitNationStats()
+    {
+        string text = "";
+        text += "Population: " + currentNation.GetPopulation() + "\n";
+        text += "Currency: " + currentNation.balance + "\n";
+
+        nationStatsText.text = text;
+    }
+
+    /// <summary>
+    /// 현재 활성화된 SubUI를 변경한다.
+    /// </summary>
+    /// <param name="index">변경할 SubUI의 index</param>
+    public void ChangeSubUI(int index)
+    {
+        currentOpenSubUI.SetActive(false);
+        subUIs[index].SetActive(true);
+        currentOpenSubUI = subUIs[index];
     }
 
     /// <summary>
